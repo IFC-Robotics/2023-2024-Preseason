880fbe9edc21ca001f3588f1707a9b74f061d01d
CharlieBrown364 <364charliebrown@gmail.com>
2022-11-08 09:01:01 -0800

    fixing auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerPlay/autonomousNoRobotClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerPlay/autonomousNoRobotClass.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@Autonomous(name="autonomous (No Robot Class)", group = "PowerPlay")
+@Autonomous(name="autonomous competition", group = "PowerPlay")
 public class autonomousNoRobotClass extends LinearOpMode {
 
     DcMotor motorFrontRight;
@@ -15,15 +15,13 @@ public class autonomousNoRobotClass extends LinearOpMode {
     DcMotor motorBackRight;
     DcMotor motorBackLeft;
     DcMotor motorLift;
-    Servo   claw;
+
+    Servo claw;
 
     double FrontRight;
     double FrontLeft;
     double BackRight;
     double BackLeft;
-    double LinearLift;
-
-
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -33,10 +31,13 @@ public class autonomousNoRobotClass extends LinearOpMode {
         telemetry.update();
 
         motorFrontRight = hardwareMap.get(DcMotor.class, "motor_front_right");
-        motorFrontLeft = hardwareMap.get(DcMotor.class, "motor_front_left");
-        motorBackLeft = hardwareMap.get(DcMotor.class, "motor_back_left");
-        motorBackRight = hardwareMap.get(DcMotor.class, "motor_back_right");
+        motorFrontLeft  = hardwareMap.get(DcMotor.class, "motor_front_left");
+        motorBackLeft   = hardwareMap.get(DcMotor.class, "motor_back_left");
+        motorBackRight  = hardwareMap.get(DcMotor.class, "motor_back_right");
+
         motorLift = hardwareMap.get(DcMotor.class, "motor_lift");
+
+        claw = hardwareMap.get(Servo.class, "servo_claw");
 
         motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
@@ -56,121 +57,116 @@ public class autonomousNoRobotClass extends LinearOpMode {
         motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        // code goes here
+
     }
-        public void drive(double speed, int distance, int direction) {
 
-            int target = direction * distance;
-            double power = direction * speed;
-            moveDriveTrain(target, target, target, target, power, power, power, power);
+    public void drive(double speed, int distance, int direction) {
+        int target = direction * distance;
+        double power = direction * speed;
+        moveDriveTrain(target, target, target, target, power, power, power, power);
+    }
 
+    public void strafe(double speed, int distance, int direction) {
+        int target = direction * distance;
+        double power = direction * speed;
+        moveDriveTrain(-target, target, target, -target, -power, power, power, -power);
+    }
+
+    public void turn(double speed, double angle) {
+
+        double circumference = 66; // what does this even mean? circumference of the wheels? must test
+        double distance = circumference*(angle/360);
+        int intDistRot = (int)inchToTics(distance);
+
+        if (angle < 0) {
+            moveDriveTrain(intDistRot, -intDistRot, intDistRot, -intDistRot, speed, -speed, speed, -speed);
+        } else if (angle >= 0) {
+            moveDriveTrain(intDistRot, -intDistRot, intDistRot, -intDistRot, -speed, speed, -speed, speed);
         }
 
+    }
 
-        public void strafe(double speed, int distance, int direction) {
+    public void lift(String target) {
 
-            int target = direction * distance;
-            double power = direction * speed;
-            moveDriveTrain(-target, target, target, -target, -power, power, power, -power);
+        double speed = 0.5;
+        double inches = 0;
 
-        }
+        if (target == "high junction") inches = 33.5;
+        else if (target == "middle junction") inches = 23.5;
+        else if (target == "low junction") inches = 13.5;
+//        else if (target == "ground junction") inches = 0;
 
-        // TURN FUNCTION BELOW. Radius = 7.7862 in; Circumference = 48.92217 in = 360 degrees
-        // 1120 ticks for 1 rotation
+        motorLift.setTargetPosition((int)inchToTics(inches));
+        motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLift.setPower(speed);
 
-        public void turn(double speed, double angle) {
+        while (motorLift.isBusy() && opModeIsActive()) {}
 
-            double circumference = 66; // 70
-            double distance = circumference*(angle/360);
-            int intDistrot = (int)inchToTics(distance);
+        motorLift.setPower(0);
+        motorLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            if (angle < 0) {
-                moveDriveTrain(intDistrot, -intDistrot, intDistrot, -intDistrot, speed, -speed, speed, -speed);
-            } else if (angle >= 0) {
-                moveDriveTrain(intDistrot, -intDistrot, intDistrot, -intDistrot, -speed, speed, -speed, speed);
-            }
+    }
 
-        }
+    public void moveClaw(String direction) {
+        if (direction == "open") claw.setPosition(0.33); // Test this later, we don't know the amount
+        if (direction == "close") claw.setPosition(0);
+    }
 
-        public void lift(double speed, double height) {
-            double ticHeight = inchToTics(height);
+    public double inchToTics(double inches) {
 
-            motorLift.setTargetPosition((int) ticHeight);
-            motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        final double COUNTS_PER_REVOLUTION    = 28.0;
+        final double GEAR_RATIO               = 20.0;
+        final double WHEEL_DIAMETER_IN_INCHES = 1.0;
+        final double COUNTS_PER_INCH = (COUNTS_PER_REVOLUTION * GEAR_RATIO) / (WHEEL_DIAMETER_IN_INCHES * Math.PI);
 
-            motorLift.setPower(speed);
+        return inches * COUNTS_PER_INCH;
 
-            while (motorLift.isBusy() && opModeIsActive()) {
+//        int tics = 1129;
+//        int dia = 4;
+//        double ratio = 0.64;
+//
+//        double wheelRot = inch / (Math.PI * dia);
+//        double motorRot = wheelRot * ratio;
+//        double finalTic = tics * motorRot;
+//        return finalTic;
 
-            }
+    }
 
-            motorLift.setPower(0);
-        }
+    public void moveDriveTrain(int frontRightTarget, int frontLeftTarget, int backRightTarget, int backLeftTarget, double frontRightSpeed, double frontLeftSpeed, double backRightSpeed, double backLeftSpeed) {
 
-        public void moveClaw() {
-            //Test this later, we don't know the amount
-            claw.setPosition(0.33);
-            lift(1, 0);
-            claw.setPosition(0);
+        motorFrontRight.setTargetPosition(frontRightTarget);
+        motorFrontLeft.setTargetPosition(frontLeftTarget);
+        motorBackRight.setTargetPosition(backRightTarget);
+        motorBackLeft.setTargetPosition(backLeftTarget);
 
-            //Lifting up to a random position, remember to change
-            double liftRandomPosition = Math.random() * 100;
-            lift(2, liftRandomPosition);
-        }
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        public double inchToTics(double inch) {
+        motorFrontRight.setPower(frontRightSpeed);
+        motorFrontLeft.setPower(frontLeftSpeed);
+        motorBackRight.setPower(backRightSpeed);
+        motorBackLeft.setPower(backLeftSpeed);
 
-//            int tics = 1129;
-//            int dia = 4;
-//            double ratio = 0.64;
-            final double COUNTS_PER_REVOLUTION    = 28.0;
-            final double GEAR_RATIO               = 20.0;
-            final double WHEEL_DIAMETER_IN_INCHES = 1.0;
-            final double COUNTS_PER_INCH = (COUNTS_PER_REVOLUTION * GEAR_RATIO) / (WHEEL_DIAMETER_IN_INCHES * Math.PI);
+        while (motorBackRight.isBusy() && opModeIsActive()) {}
 
-            return COUNTS_PER_INCH;
+        motorFrontRight.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorBackRight.setPower(0);
+        motorBackLeft.setPower(0);
 
-//            double wheelRot = inch / (Math.PI * dia);
-//            double motorRot = wheelRot * ratio;
-//            double finalTic = tics * motorRot;
-//            return finalTic;
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        }
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        public void moveDriveTrain(int frontRightTarget, int frontLeftTarget, int backRightTarget, int backLeftTarget, double frontRightSpeed, double frontLeftSpeed, double backRightSpeed, double backLeftSpeed) {
-
-            motorFrontRight.setTargetPosition(frontRightTarget);
-            motorFrontLeft.setTargetPosition(frontLeftTarget);
-            motorBackRight.setTargetPosition(backRightTarget);
-            motorBackLeft.setTargetPosition(backLeftTarget);
-
-            motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            motorFrontRight.setPower(frontRightSpeed);
-            motorFrontLeft.setPower(frontLeftSpeed);
-            motorBackRight.setPower(backRightSpeed);
-            motorBackLeft.setPower(backLeftSpeed);
-
-            while (motorBackRight.isBusy() && opModeIsActive()) {
-            }
-
-            motorFrontRight.setPower(0);
-            motorFrontLeft.setPower(0);
-            motorBackRight.setPower(0);
-            motorBackLeft.setPower(0);
-
-            motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-            motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        }
+    }
 
 }
