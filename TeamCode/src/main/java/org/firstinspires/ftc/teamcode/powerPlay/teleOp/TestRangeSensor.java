cf88efdab6e25682e0403e26c28c0f361b21a6a5
CharlieBrown364 <364charliebrown@gmail.com>
2023-02-07 09:00:39 -0800

    Elias started on writing test code for color/range sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerPlay/teleOp/TestRangeSensor.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.powerPlay.teleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.
+
+import org.firstinspires.ftc.teamcode.powerPlay.robot.Robot;
+
+@TeleOp(name="test range sensor", group="test")
+public class TestRangeSensor extends LinearOpMode {
+
+    ColorRangeSensor colorRange;
+
+    @Override
+    public void runOpMode() {
+
+        colorRange = hardwareMap.get(ColorRangeSensor.class, "color_range_sensor");
+
+        telemetry.addLine("Initializing opMode...");
+        telemetry.update();
+        Robot.init(this);
+
+        waitForStart();
+
+        telemetry.addLine("Executing opMode...");
+        telemetry.update();
+
+        while(opModeIsActive()) {
+             telemetry.addData("Red", colorRange.red());
+             telemetry.addData("Green", colorRange.green());
+        }
+
+    }
+
+}
