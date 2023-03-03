4cb13731d8de70b28e719d620030beff6633bb33
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-03-02 16:29:03 -0800

    practice autonomous test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerPlay/autonomous/PracticeAuton.java
@@ -0,0 +1,54 @@
+@Autonomous(name="Practice Autonomous", group="Linear Opmode")
+
+public class PracticeAuton extends LinearOpMode {
+
+    // Declare OpMode members.
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. 
+        frontLeftDrive  = hardwareMap.get(DcMotor.class, "motor_front_left");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "motor_front_right");
+        backLeftDrive   = hardwareMap.get(DcMotor.class, "motor_back_left");
+        backRightDrive  = hardwareMap.get(DcMotor.class, "motor_back_right");
+
+        
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
+
+        frontLeftDrive.setTarget(2000);
+        frontRightDrive.setTarget(2000);
+        backLeftDrive.setTarget(2000);
+        backRightDrive.setTarget(2000);
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        telemetry.addLine("Running autonomous");
+        telemetry.update();
+
+        frontLeftDrive.setPower(0.5);
+        frontRightDrive.setPower(0.5);
+        backLeftDrive.setPower(0.5);
+        backRightDrive.setPower(0.5);
+        
+    }
+}
