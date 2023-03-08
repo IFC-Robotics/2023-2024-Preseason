fe5752ee99370dce0d78a9ec81a18b4aa24f6036
concord01 <113149352+concord01@users.noreply.github.com>
2023-03-07 16:39:50 -0800

    fixed my stuff - J

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerPlay/autonomous/JoaquinAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powerPlay/autonomous/JoaquinAuton.java
@@ -8,18 +8,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous(name="Joaquin's Autonomous Test", group = "test")
 public class JoaquinAuton extends LinearOpMode{
 
-    DcMotor motor;
+    private DcMotor motor;
 
     public void runOpMode() {
         
         motor = hardwareMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
-
+        
+        motor.setTargetPosition(1000);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setPower(1);
-        sleep(1000);
+        while (motor.isBusy()){}
         motor.setPower(0);
-
     }
 
 }
