385173369124f05f348f77daa334eff221097565
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-08-29 16:15:25 -0700

    cup pong code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clubRush/cupPong.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clubRush/cupPong.java
@@ -1,1 +1,54 @@
+package org.firstinspires.ftc.teamcode.clubrush;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Cup Pong", group="Linear Opmode")
+
+public class cupPong extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor cupMotor;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables
+        cupMotor  = hardwareMap.get(DcMotor.class, "cup_motor");
+
+        // Wait for the turn to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run for 60 seconds
+        while (runtime.seconds() < 60 && opModeIsActive()) {
+
+            // Send calculated power to wheels
+            cupMotor.setPower(0.5);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
