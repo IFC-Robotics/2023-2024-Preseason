695a861748c8d9f8ad0621e9780d896b00ef3813
CharlieBrown364 <364charliebrown@gmail.com>
2023-08-29 17:20:05 -0700

    gradle stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clubRush/cupPong.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clubRush/cupPong.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.clubrush;
+package org.firstinspires.ftc.teamcode.clubRush;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
