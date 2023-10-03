54e3dea137e4a48d989dbb5f43ecc5caf18a971f
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-10-03 16:24:49 -0700

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clubRush/zalea.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clubRush/zalea.java
@@ -1,8 +1,32 @@
-public class zalea {
+package org.firstinspires.ftc.teamcode.clubRush;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="Basic TeleOp Code", group = "clubRush")
+public class zalea extends LinearOpMode{
     public int multiply(){
         int a = 45;
         int b = 28;
 
         return a*b
+    }
+
+    public void runOpMode() {
+
+        int i = 0;
+
+        public static DcMotor motor;
+
+        motor = hardwareMap.get(DcMotor.class, "motor_1");
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            System.out.println(i);
+            i++;
+        }
+
     }
 }
