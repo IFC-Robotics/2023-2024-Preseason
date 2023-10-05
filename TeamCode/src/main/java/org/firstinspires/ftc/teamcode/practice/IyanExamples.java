364bac29edea49c23ab844383dd1400cf477a042
Fireg53 <iyansyeedmiller@gmail.com>
2023-10-04 23:21:27 -0700

    lost duck

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/IyanExamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/IyanExamples.java
@@ -1,39 +1,66 @@
 package org.firstinspires.ftc.teamcode.practice;
 
+
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+
+//This is my (very much untested) code for finding my poor lost duck
+//It uses a color sensor to to determine if an object is yellow and displays all the other attributes for clarity
+// You might have to fiddle with my conditional statement to get a good yellow detector
+
 @Autonomous(name="IyanExamples",group="Practice")
 public class IyanExamples extends LinearOpMode{
-    public void runOpMode(){
 
-        DcMotor motor;
-        int i = 0;
+    NormalizedColorSensor colorSensor1;
 
+    @Override public void runOpMode() {
 
-        motor = hardwareMap.get(DcMotor.class,"motor_1");
+        // This increases the raw value, must be > 1
+        float gain = 2;
+
+        final float[] hsvValues = new float[3];
+
+        colorSensor1 = hardwareMap.get(NormalizedColorSensor.class, "sensor_color_1");
+
+        // If possible, turn the light on in the beginning
+        if (colorSensor1 instanceof SwitchableLight) {
+            ((SwitchableLight)colorSensor1).enableLight(true);
 
         waitForStart();
 
-        //counter that stops at 5000 loops
-        while (opModeIsActive() && i <= 5000) {
-            i += 1;
-            telemetry.addData("counter: ", i);
+        while (opModeIsActive()) {
+            colorSensor1.setGain(gain);
+
+            NormalizedRGBA colors1 = colorSensor1.getNormalizedColors();
+
+            Color.colorToHSV(colors1.toColor(), hsvValues);
+
+            telemetry.addLine()
+                    .addData("Red", "%.3f", colors1.red)
+                    .addData("Green", "%.3f", colors1.green)
+                    .addData("Blue", "%.3f", colors1.blue);
+            telemetry.addLine()
+                    .addData("Hue", "%.3f", hsvValues[0])
+                    .addData("Saturation", "%.3f", hsvValues[1])
+                    .addData("Value", "%.3f", hsvValues[2]);
+            telemetry.addData("Alpha", "%.3f", colors1.alpha);
+
+            if (colors1.red + colors1.green > 4 * colors1.blue) {
+                telemetry.addData("could that be?","my duck??");
+            }else {
+                telemetry.addData("keep searching for my duck.","I know it's around here somewhere");
+            }
+
             telemetry.update();
         }
-
-        //run motor for 5 seconds
-        motor.setPower(0.5);
-        sleep(5000);
-        motor.setPower(0);
-
-        // encoder, runs to 5000 ticks
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // finish
-
-
     }
+}
+
 
 }
