1f9ec65bfde55b0ccbbaa5a2b3807d97cc293616
megannn3 <108020187+megannn3@users.noreply.github.com>
2023-10-05 16:40:36 -0700

    Added probably not working color sensor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/megan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/megan.java
@@ -5,6 +5,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+import android.graphics.Color;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
 @Autonomous(name="Megan code", group = "Practice")
 public class megan extends LinearOpMode{
   public int multiply(){
@@ -13,6 +19,7 @@ public class megan extends LinearOpMode{
   int c = a*b;
   return c;
   }
+  NormalizedColorSensor colorSensor1;
   public void runOpMode() {
     int i = 0;
     waitForStart();
@@ -23,5 +30,63 @@ public class megan extends LinearOpMode{
       DcMotor motor;
       motor = hardwareMap.get(DcMotor.class, "motor_1");
     }
-  }
+  
+
+
+
+
+//This is my (very much untested) code for finding my poor lost duck
+//It uses a color sensor to to determine if an object is yellow and displays all the other attributes for clarity
+// You might have to fiddle with my conditional statement to get a good yellow detector
+
+
+
+    
+
+    
+
+        // This increases the raw value, must be > 1
+        float gain = 2;
+
+        final float[] hsvValues = new float[3];
+
+        colorSensor1 = hardwareMap.get(NormalizedColorSensor.class, "color_sensor_1");
+
+        // If possible, turn the light on in the beginning
+        if (colorSensor1 instanceof SwitchableLight) {
+            ((SwitchableLight) colorSensor1).enableLight(true);
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            colorSensor1.setGain(gain);
+
+            NormalizedRGBA colors1 = colorSensor1.getNormalizedColors();
+
+            Color.colorToHSV(colors1.toColor(), hsvValues);
+
+            telemetry.addLine()
+                    .addData("Red", "%.3f", colors1.red)
+                    .addData("Green", "%.3f", colors1.green)
+                    .addData("Blue", "%.3f", colors1.blue);
+            telemetry.addLine()
+                    .addData("Hue", "%.3f", hsvValues[0])
+                    .addData("Saturation", "%.3f", hsvValues[1])
+                    .addData("Value", "%.3f", hsvValues[2]);
+            telemetry.addData("Alpha", "%.3f", colors1.alpha);
+
+            if (colors1.red + colors1.green > 4 * colors1.blue) {
+                telemetry.addData("Found the duck!");
+            }else if(colors1.red > colors1.green + colors1.blue){
+                telemetry.addData("It's the cone!");
+            }else if(colors1.red = colors1.green = colors1.blue){
+                telemetry.addData("The ball!");
+            }else{
+              telemetry.addData("Keep looking")
+            }
+
+            telemetry.update();
+        }
+    }
 }
