e205244280360d32fdd7e25a3904c18a4809d515
CharlieBrown364 <364charliebrown@gmail.com>
2024-03-07 18:14:48 -0800

    ftc dash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/NoFSMteleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/NoFSMteleOp.java
@@ -30,7 +30,7 @@ public class NoFSMteleOp extends LinearOpMode {
 
             Robot.verticalLift.teleOp(-gamepad2.right_stick_y, gamepad2.right_bumper, gamepad2.a, gamepad2.x, gamepad2.b, gamepad2.y);
 
-            Robot.drivetrain.teleOp(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_bumper);
+            Robot.drivetrain.teleOp(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, false/**gamepad1.right_bumper**/);
 
             Robot.servoDeposit.teleOpManualMode(gamepad2.left_trigger > 0.2,gamepad2.right_trigger > 0.2);
             Robot.servoDeposit.teleOpAssistMode(gamepad2.dpad_down,(gamepad2.dpad_left||gamepad2.dpad_right),gamepad2.dpad_up);
@@ -39,7 +39,7 @@ public class NoFSMteleOp extends LinearOpMode {
 
 
 
-//            Robot.servoLauncher.teleOpAssistMode(gamepad2.left_bumper,false, false);
+            Robot.servoLauncher.teleOpAssistMode(gamepad1.left_bumper,false,gamepad1.right_bumper);
 
             if (gamepad1.dpad_down) {
                 pulleySpeed = 0.3F;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -44,7 +44,7 @@ public class Robot {
         drivetrain =        new Drivetrain("collector", SLEEP_TIME);
         verticalLift =      new LiftClass("motor_vertical_lift", MAX_LIFT_SPEED, 0.0005, SLEEP_TIME, true);
         servoDeposit =      new ServoClass("servo_deposit", "collect", 0.13, "score",0.5,"auton", 1, SERVO_SPEED, SERVO_TIME, false);
-//        servoLauncher =     new ServoClass("servo_launcher", "release", 0.1, "hold",0.5 ,"medium",0.65, SERVO_SPEED, SERVO_TIME, false);
+        servoLauncher =     new ServoClass("servo_launcher", "release", 0.1, "hold",0.5 ,"medium",0.65, SERVO_SPEED, SERVO_TIME, false);
         motorCollector =      new MotorClass("motor_collector", MAX_MOTOR_SPEED, SLEEP_TIME, true);
         motorLauncher =     new MotorClass("motor_launcher", 0.5*MAX_MOTOR_SPEED, SLEEP_TIME, false);
         webcam1 =           new CameraClass("webcam_1",true);
@@ -54,11 +54,10 @@ public class Robot {
         drivetrain.init(opMode);
         verticalLift.init(opMode);
         servoDeposit.init(opMode);
-//        servoLauncher.init(opMode);
+        servoLauncher.init(opMode);
         motorCollector.init(opMode);
         motorLauncher.init(opMode);
-//        webcam1.init(opMode);
-//        redBlueModel.init(opMode);
+        webcam1.init(opMode);
         motorPulley.init(opMode);
 
     }
