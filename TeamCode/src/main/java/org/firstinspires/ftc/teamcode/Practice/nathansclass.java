db0e911b97917b8972e788e86b5716f6efc150f6
CharlieBrown364 <364charliebrown@gmail.com>
2024-05-02 16:11:07 -0700

    5.2.24

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/nathansclass.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Practice;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+@Autonomous(name="nathan's autonomous", group = "Practice")
+
+public class nathansclass extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //initialization code
+        Robot.init(this);
+
+        waitForStart();
+        Robot.drivetrain.drive(12,0.6);
+        Robot.drivetrain.turn(90, 0.4) ;
+        Robot.motorCollector.runToPosition(1000);
+        Robot.motorCollector.runToPosition(-1000);
+        Robot.motorCollector.runToPosition(1000);
+        Robot.drivetrain.drive(6,0.7);
+        Robot.drivetrain.turn(90,0.5);
+        Robot.drivetrain.drive(12,0.6);
+        Robot.verticalLift.runToPosition("middle");
+        Robot.verticalLift.runToPosition("score");
+        Robot.verticalLift.runToPosition("zero");
+        Robot.drivetrain.strafe(4,0.4);
+        Robot.drivetrain.strafe(-4,0.4);
+
+
+        // start code
+    }
+}
