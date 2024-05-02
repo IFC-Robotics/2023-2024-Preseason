f4a98d195c4b6d00052fa20673e70a1291c33088
Fireg53 <iyansyeedmiller@gmail.com>
2024-05-02 16:50:03 -0700

    Created CatVideo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/CatVideo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/CatVideo.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.robot.MotorClass;
 
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
-@Autonomous(name="Basic Autonomous Code", group = "Practice")
+@Autonomous(name="Cat Video", group = "Practice")
 
 public class CatVideo extends LinearOpMode{
     @Override
@@ -17,20 +17,20 @@ public class CatVideo extends LinearOpMode{
         //initialization code
 
         waitForStart();
-        Robot.motorCollector.runToPosition(1000);
+        Robot.motorCollector.runToPosition(1000, false, 1);
         Robot.drivetrain.drive(-10,0.7);
-        Robot.motorCollector.runToPosition(1000);
+        Robot.motorCollector.runToPosition(1000, false, 1);
         Robot.drivetrain.drive(-10,0.7);
-        Robot.motorCollector.runToPosition(1000);
+        Robot.motorCollector.runToPosition(1000, false, 1);
         Robot.drivetrain.drive(-10,0.7);
-        Robot.motorCollector.runToPosition(1000);
+        Robot.motorCollector.runToPosition(1000, false, 1);
         Robot.drivetrain.strafe(-30,0.7);
         Robot.drivetrain.drive(30,0.7);
         Robot.drivetrain.strafe(10,0.7);
         Robot.verticalLift.runToPosition("middle");
         Robot.servoDeposit.runToPosition("score");
         Robot.verticalLift.runToPosition("zero",true);
-        Robot.motorCollector.runToPosition(1000);
+        Robot.motorCollector.runToPosition(1000, false, 1);
         Robot.verticalLift.runToPosition("middle");
         Robot.servoDeposit.runToPosition("score");
         Robot.verticalLift.runToPosition("zero",true);
